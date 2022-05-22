ab344402c91628a7e416e4278748af6dc9b76c8a
Aaron K <aaron.kim.dev@gmail.com>
2022-05-22 15:41:59 -0700

    main section layout

--- client/src/components/layout/main/main.jsx
+++ client/src/components/layout/main/main.jsx
@@ -1,14 +1,20 @@
 import React from "react";
+import Calendar from '../../calendar/calendar'
 import './main.scss';
 
 const Main = () => {
   return (
     <div className="main">
+      <div className="main-details">
+        {/* <Details /> */}
+        <Calendar />
+      </div>
       <div className="main-calendar">
-        {/* <Calendar /> */}
+        <Calendar />
       </div>
       <div className="main-booking">
         {/* <Booking /> */}
+        <Calendar />
       </div>
     </div>
   );
