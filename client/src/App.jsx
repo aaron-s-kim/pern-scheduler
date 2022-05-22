0720412802a53eaed9061ba4fe06a823fb2fe1a9
Aaron K <aaron.kim.dev@gmail.com>
2022-05-21 17:03:16 -0700

    setup for sass

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react';
+import Header from './components/header';
+import './sass/App.scss';
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <Header />
+        <div></div>
+        <footer className="footer">
+        </footer>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/header.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment, useState } from "react";
+import './header.scss';
+
+const Header = () => {
+  return (
+    <>
+      <h1>Hello</h1>
+    </>
+  );
+}
+
+export default Header;
